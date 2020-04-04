5b0a8aeee876e5789a4fb01d9535658e03937a9e
wujinbao <www.389092100@qq.com>
2020-04-05 01:54:35 +0800

    完成首页轮播与六板块

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import VueRouter from 'vue-router'
 // 1.2 安装路由
 Vue.use(VueRouter)
 
+// 导入 MUI 的样式
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
 
-import MintUI from 'mint-ui'
-Vue.use(MintUI)
-import 'mint-ui/lib/style.css'
+
+// 按需导入 Mint-UI 中的组件   
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+
 // 1.3 导入自己的 router.js 路由模块
 import router from './router.js'
 
@@ -19,5 +28,5 @@ import app from './App.vue'
 var vm = new Vue({
   el: '#app',
   render: c => c(app),
-  router
+  router // 1.4 挂载路由对象到 VM 实例上
 })
